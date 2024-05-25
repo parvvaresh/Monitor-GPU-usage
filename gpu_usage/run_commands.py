bba11e1ba6384e4fb5d5bb669b48607627a7195b
parvvaresh <parvvaresh@gmail.com>
2024-05-25 19:11:44 +0330

    write comment for codes

--- gpu_usage/run_commands.py
+++ gpu_usage/run_commands.py
@@ -5,7 +5,12 @@ import subprocess
 
 def run_commands(sec : int) -> None:
     """
-        write this update
+        this code have 4 steps 
+        1 -> create .sh file and write into path file csv for save result
+        2 -> run commands 
+           2.1 -> To run the script, you need to give execute permission to the file. Do this using the chmod command : chmod +x
+           2.2 -> To run the script in the background (so that it continues to run after closing the terminal), you can use the screen tool
+        3 -> Running the script in a screen session
     """
     script_path = write_sh_file(sec)
     
